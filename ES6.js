1785facf95d9a2ea343fbe6420c46b88b4912699
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2024-01-15 19:17:32 +0600

    Added new file

---
+++ ES6.js
@@ -0,0 +1,23 @@
+//learning arrow function 
+
+const doub = num=>{
+    return num*2;
+}
+
+console.log(doub(5))
+// console.log(doubleit(5))
+
+// learning template string 
+const first="Shanjida"
+
+const last="Nahid"
+
+const fullname=`${first} is a ${10+10+3} years old `
+console.log(fullname);
+
+const multiline =`javascript
+is 
+a 
+langugae`
+
+console.log(multiline)
