fafcff95063d7f4e15be6f3168b1176acc28c0ce
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2024-01-17 20:49:38 +0600

    pushed several files

---
+++ class.js
@@ -0,0 +1,13 @@
+class student {
+    constructor(id,name){
+        this.id=id;
+        this.name=name;
+        this.university="Dhaka University"
+
+    }
+}
+
+const st1=new student(100,"shorna")
+console.log(st1)
+
+// inheritance 
---
+++ three.js
@@ -0,0 +1,12 @@
+const num=[10,12,14,20]
+
+const num2=[11,15,16,17,8]
+
+const num3=[11,15,16,17,8]
+
+//concatenation
+const all_num=num.concat(num2).concat(num3)
+console.log(all_num)
+
+const numm=[...num,...num3,...num2]
+console.log(numm);
